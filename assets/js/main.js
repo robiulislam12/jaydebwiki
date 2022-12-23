5059a3e5b95db655df189c789421c1aad08651b9
Robiul Islam <robiul215565@gmail.com>
2022-12-24 03:09:33 +0600

    Faq page

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+var headers = document.querySelectorAll('.dropdown-container header');
+
+for(var i = 0; i < headers.length; i++) {
+	headers[i].addEventListener('click', openCurrAccordion);
+}
+
+//Use this as the callback if you would like multiple dropdowns to be open
+function openAccordion(e) {
+	var parent = this.parentElement;
+	var article = this.nextElementSibling;
+	
+	if (!parent.classList.contains('open')) {
+		parent.classList.add('open');
+		article.style.maxHeight = article.scrollHeight + 'px';
+	}
+	else {
+		parent.classList.remove('open');
+		article.style.maxHeight = '0px';
+	}
+}
+
+function openCurrAccordion(e) {
+	for(var i = 0; i < headers.length; i++) {
+		var parent = headers[i].parentElement;
+		var article = headers[i].nextElementSibling;
+
+		if (this === headers[i] && !parent.classList.contains('open')) {
+			parent.classList.add('open');
+			article.style.maxHeight = article.scrollHeight + 'px';
+		}
+		else {
+			parent.classList.remove('open');
+			article.style.maxHeight = '0px';
+		}
+	}
+}
